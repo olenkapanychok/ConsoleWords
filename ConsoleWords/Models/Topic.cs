6482f3f70cf4c8068908c463a022b4dd3fcb5818
Helen <olenkapanychok@gmail.com>
2023-03-12 21:11:25 +0200

    added topic

---
+++ ConsoleWords/Models/Topic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace words.Models
+{
+    internal class Topic
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+       
+        public Topic(Guid id, string title)
+        {
+            Id = id;
+            Title = title;
+            
+        }
+    }
+}
