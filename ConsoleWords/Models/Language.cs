fe214b680b31b77b5bd8b3776196a4be45d52f18
Helen <olenkapanychok@gmail.com>
2023-03-12 20:53:57 +0200

    reset mixed of models

--- ConsoleWords/Models/Language.cs
+++ ConsoleWords/Models/Language.cs
@@ -11,19 +11,18 @@ namespace words.Models
         public Guid Id { get; set; }
         public string Title { get; set; }
         public List<Word> Words { get; set; }
-		public string Description { get; set; }
+        public String Description { get; set; }
 
-        public Language(Guid id, string title, List<Word> words, string description)
+        public Language(Guid id, string title, List<Word> words, string description, List<Group> groups)
         {
             Id = id;
             Title = title;
             Words = words;
-			Description = description;
+            Description = description;
         }
 
         public double getAverageLevel()
         {
-
             return Words.Sum(x => x.Level) / Words.Count;
         }
 
