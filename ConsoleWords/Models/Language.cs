8b073202bff4d967fc61e2b87d74803914fa95f0
Helen <olenkapanychok@gmail.com>
2023-03-12 20:27:41 +0200

    fixed models by g2

--- ConsoleWords/Models/Language.cs
+++ ConsoleWords/Models/Language.cs
@@ -11,19 +11,20 @@ namespace words.Models
         public Guid Id { get; set; }
         public string Title { get; set; }
         public List<Word> Words { get; set; }
-		public string Description { get; set; }
+		public String Description { get; set; }
+        public List<Group> Groups { get; set; }
 
-        public Language(Guid id, string title, List<Word> words, string description)
+        public Language(Guid id, string title, List<Word> words, string description, List<Group> groups)
         {
             Id = id;
             Title = title;
             Words = words;
 			Description = description;
+            Groups = groups;
         }
 
         public double getAverageLevel()
         {
-
             return Words.Sum(x => x.Level) / Words.Count;
         }
 
