7d1c839c22875e30d321904fa521471fc843aaba
Helen <olenkapanychok@gmail.com>
2023-03-12 21:12:12 +0200

    added question

---
+++ ConsoleWords/Models/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace words.Models
+{
+    internal class Question
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+       
+        public Question(Guid id, string title)
+        {
+            Id = id;
+            Title = title;
+            
+        }
+    }
+}
